df868b4b8a64f5e2821372df4082f421106b09e9
Shrey Bhatt <bhatt.shrey1224@gmail.com>
2022-03-04 11:52:42 +0530

    Added initial bottom nav

--- app/src/main/java/com/example/socialmediacloneapp/MainActivity.kt
+++ app/src/main/java/com/example/socialmediacloneapp/MainActivity.kt
@@ -3,11 +3,24 @@ package com.example.socialmediacloneapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
+//todo
+// add bottom navigation
+// add firebase
+// add login feature
+// add hilt
+// Design how data will be stored for each user in firebase
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val myBottomNav=findViewById<BottomNavigationView>(R.id.my_bottom_nav)
+        val navController=findNavController(R.id.my_nav_host_fragment)
+        myBottomNav.setupWithNavController(navController)
     }
 }
